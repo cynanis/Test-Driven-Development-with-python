bca45a4c207a39309791db4359cf0f48d4ab40ba
anisDev <anismehrab@gmail.com>
2019-01-03 11:21:13 +0100

    testing the implementation instead of testing the constant

--- lists/tests.py
+++ lists/tests.py
@@ -2,19 +2,13 @@ from django.urls import resolve
 from django.test import TestCase
 from  lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-
-        found = resolve('/')
-        self.assertEqual(found.func,home_page)
 
     def test_home_page_returns_correct_html(self):
-
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>',html)
-        self.assertTrue(html.endswith('</html>'))
+        #instead of creating httprequenst and calling the view function
+        #we call self.client.get , passing it the URL we want to test
+        response = self.client.get('/')
+        self.assertTemplateUsed(response,'home.html')
